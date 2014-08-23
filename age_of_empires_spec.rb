14f8565c6edd698ea709d0be39271462e67a59c0
Pablo Romanelli <pabloromanelli@gmail.com>
2014-08-22 21:08:02 -0300

    Test para clase 2

--- age_of_empires_spec.rb
+++ age_of_empires_spec.rb
@@ -66,6 +66,22 @@ class Espadachin < Guerrero
   end
 end
 
+class Mago
+  attr_accessor :teletransportando
+
+  def initialize
+    @teletransportando = []
+  end
+
+  def curar(unidad)
+    unidad.energia += 20
+  end
+
+  def teletransportar(unidad)
+    @teletransportando << unidad
+  end
+end
+
 describe 'Age of Empires' do
 
   it 'un guerrero ataca a otro' do
@@ -131,5 +147,86 @@ describe 'Age of Empires' do
     }.to raise_error NoMethodError
 
   end
+
+  # ===================================================
+
+  # it 'cuando un misil descansa ataca doble' do
+  #   muralla = Muralla.new
+  #   misil = Misil.new
+  #   misil.descansar
+  #
+  #   misil.ataca_a(muralla)
+  #
+  #   muralla.energia.should == (1000 - 120)
+  # end
+  #
+  # it 'cuando una muralla descansa suma 10 de energia' do
+  #   muralla = Muralla.new
+  #
+  #   muralla.descansar
+  #
+  #   muralla.energia.should == 1010
+  # end
+  #
+  # it 'un gerrero cuando descansa genera el doble de daño y gana 10 de energía' do
+  #   conan = Guerrero.new
+  #   muralla = Muralla.new
+  #
+  #   conan.descansar
+  #   conan.ataca_a(muralla)
+  #
+  #   muralla.energia.should == 1000 - 60
+  #   conan.energia.should == 110
+  # end
+  #
+  # it 'un kamikaze debe descansar como atacante pero no como defensor' do
+  #   kamikaze = Kamikaze.new
+  #   energia_original = kamikaze.energia
+  #   muralla = Muralla.new
+  #
+  #   kamikaze.descansar
+  #   kamikaze.energia.should == energia_original
+  #
+  #   kamikaze.ataca_a(muralla)
+  #   muralla.energia.should == 40
+  #   kamikaze.energia.should == 0
+  # end
+  #
+  # # --------------------------------------
+  #
+  # it 'un ejercito cobarde se retira cuando cualquiera de sus unidades es atacada' do
+  #   ejercito = EjercitoCobarde.new
+  #   conan = Guerrero.new
+  #   atila = Guerrero.new
+  #   ejercito.agregar_unidad(conan)
+  #
+  #   atila.ataca_a(conan)
+  #
+  #   ejercito.retirado?.should == true
+  # end
+  #
+  # it 'un ejercito descansador hace descansar una unidad cuando es atacada' do
+  #   ejercito = EjercitoDescansador.new
+  #   conan = Guerrero.new
+  #   atila = Guerrero.new
+  #   ejercito.agregar_unidad(conan)
+  #
+  #   atila.ataca_a(conan)
+  #
+  #   conan.energia.should == (100 - 20 + 10)
+  # end
+  #
+  # # ---------------------------------------
+  #
+  # it 'cuando atila es atacado el mago lo cura' do
+  #   mago = Mago.new
+  #   atila = Guerrero.new
+  #   # asociar mago con atila
+  #   conan = Guerrero.new
+  #
+  #   conan.atacar_a(atila)
+  #
+  #   atila.energia.should == (100 - 20 + 20)
+  # end
 
 end
